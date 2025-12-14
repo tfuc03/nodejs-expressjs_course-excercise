720ed5302530f604e2a731affa796485829007d1
Tfuc03 <phamphucdev1n@gmail.com>
2025-12-14 18:04:13 +0700

    Update exc(8-11) - Finished Course

---
+++ ExpressJs/8-middleware-basic.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+const logger = (req, res, next ) =>{
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    next()
+}
+
+app.get('/', logger, (req, res)=>{
+    res.status(200).send('Home Page')
+})
+
+app.get('/about', logger, (req, res)=>{
+    res.status(200).send('About Page')
+})
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port: 5000...')
+})
